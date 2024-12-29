8e8d9b81955411dd1b9000ac449480b1cbf0add7
Explik <90565470+Explik@users.noreply.github.com>
2024-12-29 11:29:44 +0100

    Adding missing implementation details

--- exercises-for-lectures/exercise-1/sketch.js
+++ exercises-for-lectures/exercise-1/sketch.js
@@ -1,19 +1,20 @@
-let destinationInput; 
+let destinationInput;
 let durationInput;
 let messageOutput;
+let activityOutput;
 
 const beach = 'strand';
 const mountain = 'bjerge';
 const city = 'by';
-const countrySide = 'landet';
+const countrySide = 'land';
 const destinations = [beach, mountain, city, countrySide];
 
 function setup() {
-    noCanvas(); 
+    noCanvas();
 
     // Create and place the labels, inputs and buttons
     const column1 = 10, column2 = 120, column3 = 320;
-    const row1 = 10, row2 = 50, row3 = 90;
+    const row1 = 10, row2 = 50, row3 = 90, row4 = 130;
 
     createSpan('Destination: ').position(column1, row1);
     destinationInput = createInput().position(column2, row1);
@@ -24,41 +25,52 @@ function setup() {
 
     messageOutput = createInput().position(column2, row3).attribute("disabled", "");
 
+    createSpan('Forslag').position(column1, row4);
+    activityOutput = createInput().position(column2, row4).attribute("disabled", "");
+
     // Attach event listeners
-    destinationInput.changed(updateMessage);
-    durationInput.changed(updateMessage);
+    destinationInput.changed(updateActivityAndMessage);
+    durationInput.changed(updateActivityAndMessage);
 }
 
-function updateMessage() {
+function updateActivityAndMessage() {
     // Fetch inputs
     const input = readInput();
 
-    // Validate input
-    const error = validateInput();
-    if (error) {
-        messageOutput.value(error);
+    // Validate input, return if any errors are found
+    const destinationError = validateDestinationInput(input);
+    if (destinationError) {
+        messageOutput.value(destinationError);
+        return;
+    }
+    
+    // Generate activity (requires valid destination)
+    const activity = generateActivity(input.destination);
+    activityOutput.value(activity);
+
+    // Validate input, return if any errors are found
+    const durationError = validateDurationInput(input);
+    if (durationError) {
+        messageOutput.value(durationError);
         return;
     }
 
-    // Generate message
-    const message = generateMessage(input);
+    // Generate message (requires valid destination and duration)
+    const message = generateMessage(input.destination, input.duration);
     messageOutput.value(message);
-} 
+}
 
-function generateMessage(input) {
-    const { destination, duration } = input;
-
-    switch (destination) {
-        case beach:
-            return generateBeachMessage(duration);
-        case mountain:
-            return generateMountainMessage(duration);
-        case city:
-            return generateCityMessage(duration);
-        case countrySide:
-            return generateCountrySideMessage(duration);
-        default:
-            return 'Fejl: Ukendt destination';
+function generateMessage(destination, duration) {
+    if (destination === beach) {
+        return generateBeachMessage(duration);
+    } else if (destination === mountain) {
+        return generateMountainMessage(duration);
+    } else if (destination === city) {
+        return generateCityMessage(duration);
+    } else if (destination === countrySide) {
+        return generateCountrySideMessage(duration);
+    } else {
+        return 'Fejl: Ukendt destination';
     }
 }
 
@@ -67,7 +79,7 @@ function generateBeachMessage(duration) {
     // 3 ≤ Længde ≤ 7 → "Perfekt til en uges afslapning. Husk bøger og badetøj!"
     // Længde > 7 → "En længere strandferie. Overvej også nogle udflugter."
 
-    if (duration < 3) 
+    if (duration < 3)
         return 'En kort strandtur. Husk badetøj og solcreme!';
     else if (duration <= 7)
         return 'Perfekt til en uges afslapning. Husk bøger og badetøj!';
@@ -79,25 +91,25 @@ function generateMountainMessage(duration) {
     // Længde < 3 → "En kort tur. Sørg for at planlægge dine vandreture."
     // 3 ≤ Længde ≤ 7 → "En uge i bjergene. Husk varmt tøj og gode sko!"
     // Længde > 7 → "Overvej længere ruter og ekstra proviant."
-    if (duration < 3) 
+    if (duration < 3)
         return 'En kort tur. Sørg for at planlægge dine vandreture.';
     else if (duration <= 7)
         return 'En uge i bjergene. Husk varmt tøj og gode sko!';
     else
         return 'Overvej længere ruter og ekstra proviant.';
-}   
+}
 
 function generateCityMessage(duration) {
     // Længde < 3 → "En kort bytur. Fokusér på de vigtigste seværdigheder."
     // 3 ≤ Længde ≤ 7 → "Perfekt til at udforske byens kultur og mad."
-	// Længde > 7 → "Overvej at tage på dagsture uden for byen."
+    // Længde > 7 → "Overvej at tage på dagsture uden for byen."
     if (duration < 3)
         return 'En kort bytur. Fokusér på de vigtigste seværdigheder.';
     else if (duration <= 7)
         return 'Perfekt til at udforske byens kultur og mad.';
     else
         return 'Overvej at tage på dagsture uden for byen.';
-}   
+}
 
 function generateCountrySideMessage(duration) {
     // Længde < 3 → "En smuttur. Nyd roen på landet."
@@ -111,24 +123,46 @@ function generateCountrySideMessage(duration) {
         return 'Planlæg længere gåture eller cykelture.';
 }
 
-function validateInput() {
-    const input = readInput();
+function generateActivity(destination) {
+    // "strand" → "Tag en svømmetur og slap af."
+    // "bjerge" → "Planlæg en vandretur."
+    // "by" → "Udforsk lokale museer og caféer."
+    // "land" → "Tag en cykeltur og nyd naturen."
+    switch(destination) {
+        case beach:
+            return 'Tag en svømmetur og slap af.';
+        case mountain:
+            return 'Planlæg en vandretur.';
+        case city:
+            return 'Udforsk lokale museer og caféer.';
+        case countrySide:
+            return 'Tag en cykeltur og nyd naturen.';
+        default:
+            return 'Fejl: Ukendt destination';
+    }
+} 
 
+function validateDestinationInput(input) {
     // Validate destination, return any error if found
-    if (!input.rawDestination) 
+    if (!input.rawDestination)
         return 'Fejl: Udfyld venligst destination';
-    
+
     if (!destinations.includes(input.destination))
         return 'Fejl: Ukendt destination';
 
+    // Return no error
+    return undefined;
+}
+
+function validateDurationInput(input) {
     // Validate duration, return any error if found
-    if (!input.rawDuration) 
+    if (!input.rawDuration)
         return 'Fejl: Udfyld venligst varighed';
-    
-    if (isNaN(input.duration)) 
+
+    if (isNaN(input.duration))
         return 'Fejl: Varighed skal være et tal';
-    
-    if (input.duration < 1) 
+
+    if (input.duration < 1)
         return 'Fejl: Varighed skal være mindst 1';
 
     // Return no error 
@@ -137,8 +171,8 @@ function validateInput() {
 
 function readInput() {
     const rawDestination = destinationInput.value();
-    const destination = rawDestination.toLowerCase();
-    
+    const destination = rawDestination && rawDestination.toLowerCase().trim();
+
     const rawDuration = durationInput.value();
     const duration = parseInt(rawDuration);
 
